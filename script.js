b87e8667b8eb86f4c3cee823fc0bfabb779f34b4
GabrielePassoni <gabriele.passoni@mail.polimi.it>
2023-10-15 17:21:38 +0200

    big update

--- script.js
+++ script.js
@@ -12,10 +12,21 @@ window.onload = function () {
         img.src = "./src/Personal.JPG";
         img.onload = function () {
             contentBox.innerHTML = `<div class="presentationcontainer">
-         <img id="mainimg" src="./src/Personal.JPG" loading="eager">
-         <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti explicabo dicta delectus distinctio sed numquam, aut laborum odit, tempore quasi cupiditate, repellendus ex quod facere ipsam minima repellat cumque temporibus?</div>
-         </div>`
-         display();
+            <img id="mainimg" src="./src/Personal.JPG" loading="eager">
+            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti explicabo dicta delectus distinctio
+                sed numquam, aut laborum odit, tempore quasi cupiditate, repellendus ex quod facere ipsam minima
+                repellat cumque temporibus?</div>
+            <div id="contactpanel">
+                <a href=""><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></a>
+                <a href=""><img
+                        src="//upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"></a>
+                <a href=""><img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png"></a>
+                <a href=""><img
+                        src="https://static.vecteezy.com/system/resources/previews/009/973/213/original/email-and-mail-icon-sign-symbol-design-free-png.png"></a>
+            </div>
+        </div>`
+            display();
         }
     })
 
@@ -24,8 +35,29 @@ window.onload = function () {
         homebutton.classList.remove("pressedbutton");
         aboutmebutton.classList.remove("pressedbutton");
         contentBox.innerHTML = ``
+        contentBox.querySelectorAll(".projectdiv").forEach((e, n, l) => {
+            e.addEventListener("click", function (event) {
+                if (e.classList.contains("selectedp")) {
+                    e.classList.remove("selectedp");
+                } else {
+                    e.classList.add("selectedp");
+                    e.querySelector(".pdescription").classList.add("visibledescription");
+                }
+            })
+        })
         contentBox.querySelector("div").classList.add("appear");
     })
+
+    contentBox.querySelectorAll(".projectdiv").forEach((e, n, l) => {
+            e.addEventListener("click", function (event) {
+                if (e.classList.contains("selectedp")) {
+                    e.classList.remove("selectedp");
+                } else {
+                    e.classList.add("selectedp");
+                    e.querySelector(".pdescription").classList.add("visibledescription");
+                }
+            })
+        })
 
     document.getElementById("aboutmebutton").addEventListener("click", function (e) {
         this.classList.add("pressedbutton");
@@ -54,6 +86,6 @@ window.onload = function () {
         contentBox.querySelector("div").classList.add("appear");
     }
 
-    document.getElementById("homebutton").click();
+    //document.getElementById("homebutton").click();
 }
 
