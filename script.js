793f54f3308cd0e35e0189e9de0549f4d4ba8189
GabrielePassoni <gabriele.passoni@mail.polimi.it>
2024-04-16 16:08:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ window.onload = function () {
         /*let img = new window.Image()
         img.src = "./src/Personal.jpg"
         img.onload = function () {*/
-            load("https://gabrielepassoni.github.io/home.html").then(value => {
-                contentBox.innerHTML = value
-                display()
-            })
+        load("https://gabrielepassoni.github.io/home.html").then(value => {
+            contentBox.innerHTML = value
+            display()
+        })
         //}
     })
 
@@ -56,16 +56,28 @@ window.onload = function () {
     }
 
     function load(httpsString) {
-        return new Promise((resolve, reject) => {
+        return new Promise(resolve => {
             let request = new XMLHttpRequest()
             request.onreadystatechange = () => {
-                if (request.readyState == request.DONE)
-                    resolve(request.responseText)
+                if (request.readyState == request.DONE) {
+                    let reqDoc = request.response
+                    let images = [...reqDoc.querySelectorAll("img")]
+                    Promise.all(images.map(img => new Promise(resolve => img.onload = resolve)))
+                        .then(() => resolve(request.responseText))
+                }
             }
             request.open("GET", httpsString)
             request.send()
         })
     }
+
+    const images = [...document.querySelectorAll("div img")];
+
+    // list all image widths and heights _after_ the images have loaded:
+    Promise.all(images.map(im => new Promise(resolve => im.onload = resolve))).then(() => {
+        console.log("The images have loaded at last!\nHere are their dimensions (width,height):");
+        console.log(images.map(im => ([im.width, im.height])));
+    })
 
     document.getElementById("homebutton").click()
 }
